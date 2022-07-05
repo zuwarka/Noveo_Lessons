7eb24488a6ee98d504b977febea140143144a7b9
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-07-05 14:20:04 +0300

    Create test.rb
    Recursive func for finding sum of elements of an array and examples

---
+++ homework01 (04.07.2022)/test.rb
@@ -0,0 +1,24 @@
+def sum(my_array)
+  if my_array.length == 1
+    return my_array[0]
+  end
+  my_array.shift + sum(my_array)
+end
+
+array_first = [0, 1, 2, 3, 4, 5]
+puts "The first array and its sum: "
+puts sum(array_first)
+
+array_second = [-2, 0, 3, -3]
+puts "The second array and its sum: "
+puts sum(array_second)
+
+array_third = [5]
+puts "The third array and its sum: "
+puts sum(array_third)
+
+'''
+array_forth = [1, "sample", 42]
+puts "The forth array and its sum: "
+puts sum(array_forth)
+'''
