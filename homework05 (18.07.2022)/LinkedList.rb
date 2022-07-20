50c7c928a76833f88adedf9edee57e1e45a73a57
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-07-21 00:30:48 +0300

    Create LinkedList.rb

---
+++ homework05 (18.07.2022)/LinkedList.rb
@@ -0,0 +1,93 @@
+# Нужно реализовать класс LinkedList, в котором можно будет хранить элементы класса Node.
+# У класса должны быть методы для добавления в конец и произвольную позицию, удаления и поиска элемента.
+# Классы должны быть описаны в разных файлах. Примеры:
+#
+#
+# list = LinkedList.new
+#
+# list.append(3)
+# list.append(5)
+# list.append(10)
+# puts list #=> (3, 5, 10)
+#
+# list.append_after(3, 15)
+# puts list #=> (3, 15, 5, 10)
+# list.append_after(10, 25)
+# puts list #=> (3, 15, 5, 10, 25)
+#
+# list.delete(10)
+# list.find(25)
+#
+# У класса должен быть реализован метод to_s для лаконичного отображения.
+
+require_relative 'Node'
+
+class LinkedList
+  #attr_accessor :head, :size, :tail
+
+  def initialize
+    @head = nil
+  end
+
+  def find_tail
+    node = @head
+    return node unless node.next_node
+    return node unless node.next_node while (node = node.next_node)
+  end
+
+  def find(data)
+    node = @head
+    return false unless node.next_node
+    return node if node.data == data
+
+    while (node = node.next_node)
+      return node if node.data == data
+    end
+  end
+
+  def find_before(data)
+    node = @head
+    return false unless node.next_node
+    return node if node.next_node.data == data
+
+    while (node = node.next_node)
+      return node if node.next_node && node.next_node.data == data
+    end
+  end
+
+  def append(data)
+    if @head
+      find_tail.next_node = Node.new(data)
+    else
+      @head = Node.new(data)
+    end
+  end
+
+  def append_after(element, data)
+    node = find(element)
+    return unless node
+
+    old = node.next_node
+    node.next_node = Node.new(data)
+    node.next_node.next_node = old
+  end
+
+  def delete(data)
+    if @head.data == data
+      @head = @head.next_node
+      return
+    end
+
+    node = find_before(data)
+    node.next_node = node.next_node.next_node
+  end
+
+  def to_s
+    node = @head
+    print "(", node.data
+    while (node = node.next_node)
+      print ", ", node.data
+    end
+    print ")"
+  end
+end
