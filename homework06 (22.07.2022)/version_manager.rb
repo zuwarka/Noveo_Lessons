a6ae20abf9477bc7890686c5960289e523b76272
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-07-30 12:33:18 +0300

    Update version_manager.rb
    Вместо расширения String прописан новый класс VersionValidator, в котором проверяем значения версии на целые числа и на, собственно, целые числа (Integer). В VersionManager'e создаем объект класса VersionValidator и вызываем метод check_integer.
    Сделан рефакторинг метода rollback! - условие ошибки (строка 58) записано в одну строку.

--- homework06 (22.07.2022)/version_manager.rb
+++ homework06 (22.07.2022)/version_manager.rb
@@ -4,24 +4,36 @@
 # -- Если часть версии - не целое число (напр. "а.4.е"), то генерировать ошибку.
 # -- Если версия не указана, то значение по умолчанию - “0.0.1”.
 
-class String
-  def is_integer?
+class VersionValidator
+  def initialize(valid_version)
+    @valid_version = valid_version
+  end
+
+  def is_integer?(split_version)
     [                          # In descending order of likeliness:
-      /^[-+]?[0-9]([0-9]*)?$/, # decimal
+     /^[-+]?[0-9]([0-9]*)?$/, # decimal
       /^0[0-7]+$/,             # octal
       /^0x[0-9A-Fa-f]+$/,      # hexadecimal
       /^0b[01]+$/              # binary
     ].each do |match_pattern|
-      return true if self =~ match_pattern
+      return true if split_version =~ match_pattern
     end
     return false
+  end
+
+  def check_integer
+    unless is_integer?(@valid_version.split('.')[0]) &&
+      is_integer?(@valid_version.split('.')[1]) &&
+      is_integer?(@valid_version.split('.')[2])
+     raise 'Invalid version'
+   end
   end
 end
 
 class VersionManager
   def initialize(m_version = "0.0.1")
+    VersionValidator.new(m_version).check_integer
     @prev_version = m_version
-    check_integer
     @major = m_version.split(/[.]/)[0].to_i
     @minor = m_version.split(/[.]/)[1].to_i
     @patch = m_version.split(/[.]/)[2].to_i
@@ -43,9 +55,7 @@ class VersionManager
   end
 
   def rollback!
-    if @prev_version == nil
-      raise 'Can not rollback to previous version'
-    end
+    raise 'Can not rollback to previous version' if @prev_version.nil?
     @major = @prev_version.split(/[.]/)[0].to_i
     @minor = @prev_version.split(/[.]/)[1].to_i
     @patch = @prev_version.split(/[.]/)[2].to_i
@@ -53,14 +63,5 @@ class VersionManager
 
   def release
     "#{@major}.#{@minor}.#{@patch}"
-  end
-
-  private
-  def check_integer
-    unless @prev_version.split(/[.]/)[0].is_integer? &&
-      @prev_version.split(/[.]/)[1].is_integer? &&
-      @prev_version.split(/[.]/)[2].is_integer?
-      raise 'Invalid version'
-    end
   end
 end
