6f0d6368f42e6bf4cd0c239bba915981141ae419
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-08-15 11:35:50 +0300

    Update Node.rb
    Добавлены методы reverse_node и reverse_node! для рекурсивной работы таких же методов в Списке.

--- homework05 (18.07.2022)/Node.rb
+++ homework05 (18.07.2022)/Node.rb
@@ -8,4 +8,21 @@ class Node
   def to_s
     @data.to_s
   end
+  # -------------------------------------------------- Добавлено от 12.08.22
+  def reverse_node(reversed = nil)
+    if next_node
+      next_node.reverse_node(Node.new(data, reversed))
+    else
+      Node.new(data, reversed)
+    end
+  end
+
+  def reverse_node!(node = self, reversed_list = reverse_node)
+    self.data = reversed_list.data
+    if next_node
+      next_node.reverse_node!(node, reversed_list.next_node)
+    else
+      node
+    end
+  end
 end
