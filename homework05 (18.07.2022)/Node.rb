380e1fc9c4d960546127bee4db9297367da0829d
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-08-15 11:38:09 +0300

    Update Node.rb
    Отредактирован конструктор

--- homework05 (18.07.2022)/Node.rb
+++ homework05 (18.07.2022)/Node.rb
@@ -1,7 +1,7 @@
 class Node
   attr_accessor :next_node, :data
-  def initialize(data)
-    @next_node = nil
+  def initialize(data, next_node = nil)
+    @next_node = next_node
     @data = data
   end
 
