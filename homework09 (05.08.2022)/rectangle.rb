2722ed581232633833738494e272a685d46a9622
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-08-18 18:26:45 +0300

    Update rectangle.rb
    Убран attr_

--- homework09 (05.08.2022)/rectangle.rb
+++ homework09 (05.08.2022)/rectangle.rb
@@ -1,7 +1,6 @@
 require_relative 'figures'
 
 class Rectangle < Figure
-  attr_writer :rectangle_a, :rectangle_b
   def initialize(length, width)
     @rectangle_a = length
     @rectangle_b = width
