bb8238fbfdb6be6930d82c68601149cc2baa29f0
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-08-18 18:26:16 +0300

    Update figures.rb
    Убран метод to_s, добавлена библиотека Comparable

--- homework09 (05.08.2022)/figures.rb
+++ homework09 (05.08.2022)/figures.rb
@@ -1,14 +1,11 @@
 class Figure
   attr_accessor :area
+  include Comparable
   def initialize(my_area)
     @area = my_area
   end
 
   def <=>(other_shape)
     self.area <=> other_shape.area
-  end
-
-  def to_s
-    "#{@area}"
   end
 end
