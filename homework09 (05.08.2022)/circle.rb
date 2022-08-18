5ec9fabec6b216b7f4de30f95e1b940160e09ba2
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-08-18 18:24:35 +0300

    Update circle.rb
    Убран include Math и attr_

--- homework09 (05.08.2022)/circle.rb
+++ homework09 (05.08.2022)/circle.rb
@@ -1,10 +1,8 @@
 require_relative 'figures'
 
 class Circle < Figure
-  include Math
-  attr_writer :circle_r
   def initialize(radius)
     @circle_r = radius
-    @area = PI * (@circle_r ** 2)
+    @area = Math::PI * (@circle_r ** 2)
   end
 end
