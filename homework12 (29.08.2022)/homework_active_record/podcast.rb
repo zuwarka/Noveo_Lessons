762467093727ee14fb33858aa226dd175124050f
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-09-07 13:45:51 +0300

    Update podcast.rb

--- homework12 (29.08.2022)/homework_active_record/podcast.rb
+++ homework12 (29.08.2022)/homework_active_record/podcast.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'active_record'
 require_relative 'subscription'
 require_relative 'user'
@@ -8,9 +10,26 @@ ActiveRecord::Base.establish_connection(
 )
 
 class Podcast < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: true
-  enum type: [ :society, :news, :sport, :business, :culture ]
+  enum type: %i[society news sport business culture]
 
-  has_many :subscriptions, as: :subscribable
-  has_many :users, through: :subscription
+  validates :name, presence: true, uniqueness: true
+  validates :type, inclusion: { in: type.keys }
+
+  has_many :subscriptions, as: :subscribable, dependent: :destroy
+  has_many :users, through: :subscription, dependent: :destroy
+end
+
+class CreatePodcastTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :podcasts do |t|
+      t.string :name, null: false, uniq: true
+      t.integer :type, null: false
+
+      t.timestamps
+    end
+  end
+end
+
+unless ActiveRecord::Base.connection.table_exists?('podcasts')
+  CreatePodcastTable.migrate(:up)
 end
