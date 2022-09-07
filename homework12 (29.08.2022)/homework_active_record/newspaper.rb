a2848ffb77864a9f1e87e63ac077cf56e654ce82
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-09-07 13:45:13 +0300

    Update newspaper.rb

--- homework12 (29.08.2022)/homework_active_record/newspaper.rb
+++ homework12 (29.08.2022)/homework_active_record/newspaper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'active_record'
 require_relative 'subscription'
 require_relative 'user'
@@ -6,11 +8,27 @@ ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
   database: 'test.db'
 )
-
 class Newspaper < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: true
-  enum type: [ :news, :policy, :sport, :science, :university ]
+  enum type: %i[news policy sport science university]
 
-  has_many :subscriptions, as: :subscribable
-  has_many :users, through: :subscription
+  validates :type, inclusion: { in: type.keys }
+  validates :name, presence: true, uniqueness: true
+
+  has_many :subscriptions, as: :subscribable, dependent: :destroy
+  has_many :users, through: :subscription, dependent: :destroy
+end
+
+class CreateNewspaperTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :newspaper do |t|
+      t.string :name, null: false, uniq: true
+      t.integer :type, null: false
+
+      t.timestamps
+    end
+  end
+end
+
+unless ActiveRecord::Base.connection.table_exists?('newspapers')
+  CreateNewspaperTable.migrate(:up)
 end
