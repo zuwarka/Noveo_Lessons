5f39cce8e8c002735f82b98ae481bcb2301dc284
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-09-07 13:47:11 +0300

    Update user.rb

--- homework12 (29.08.2022)/homework_active_record/user.rb
+++ homework12 (29.08.2022)/homework_active_record/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'active_record'
 require_relative 'subscription'
 
@@ -10,4 +12,33 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
 
   has_many :subscriptions, dependent: :destroy
+
+  before_update :update_subscriptions
+
+  private
+
+  def update_subscriptions
+    return if active
+    Subscription.each { |subscription| subscription.disabled! if subscription.user_id == id }
+  end
+end
+
+class CreateUserTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false, uniq: true
+      t.timestamps
+    end
+  end
+end
+
+class AddActiveToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :active, :boolean, null: false, default: true
+  end
+end
+
+unless ActiveRecord::Base.connection.table_exists?('users')
+  CreateUserTable.migrate(:up)
+  AddActiveToUser.migrate(:up)
 end
